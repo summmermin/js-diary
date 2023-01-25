a31743bf90755ede0cf88dc6c54464bcaae7ec39
summmermin <110873921+summmermin@users.noreply.github.com>
2023-01-25 13:51:56 +0900

    json

--- json/json.js
+++ json/json.js
@@ -1,20 +1,35 @@
-const summer = {
-    name: "summer",
-    hobby: ["dance", "cooking"],
-    pet: "kong",
-    num: 1
+// 1. object to json
+// stringify(obj)
+let json = JSON.stringify(true);
+console.log(json) //true
+json = JSON.stringify(['apple', 'banana']);
+console.log(json) // ["apple","banana"]
+
+let dog = {
+    name: 'ming',
+    age: 5,
+    size: null,
+    birthdate: new Date(),
+    jump: () => {
+        console.log(`${name} can jump!`)
+    }
 }
 
-const jsonSummer = JSON.stringify(summer)
-console.log(typeof jsonSummer, jsonSummer)
-// "string" '{"name":"summer","hobby":["dance","cooking"],"pet":"kong","num":1}'
+console.log(dog) // 함수는 오브젝트 안의 데이터가 아니어서 함수는 제외되서 나옴, 자바스크립트 만의 특별한 데이터도 안들어감
+json = JSON.stringify(dog, ['name']) //뒤에 해당하는 프로퍼티만 정의되어서 나오게 된다 {"name":"ming"}
+json = JSON.stringify(dog, (key, value) => {
+    console.log(`key:${key}, value:${value}`);
+    return key === 'name' ? 'summer' : value;
+})
 
-const todos = [
-    {id: 1, content: 'css', alive: true}, {id: 2, content: 'hmtl', alive: true}
-]
-
-const todosJson = JSON.stringify(todos)
-console.log(typeof todosJson, todosJson)
-//"string" '[{"id":1,"content":"css","alive":true},{"id":2,"content":"hmtl","alive":true}]'
-console.log(typeof parseTodos, parseTodos)
-// "object" Array [Object { id: 1, content: "css", alive: true }, Object { id: 2, content: "hmtl", alive: true }]
+//2.json to object
+//parse(json)
+console.log(clear);
+json = JSON.stringify(dog);
+const obj = JSON.parse(json, (key, value) => {
+    console.log(json)
+    return key === 'birthdate' ? new Date(value) : value;
+});
+console.log(obj)
+// obj.jump() 이거 안들어있다. stringfy 하면서 함수가 제외되기 때문에 다시 parse 할때도 없음
+console.log(obj.birthdate.getDate());
